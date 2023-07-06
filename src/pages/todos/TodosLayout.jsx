a19138b718f0ba5258599243070d876c90922475
aagic1 <aagic1@etf.unsa.ba>
2023-07-06 19:30:10 +0200

    Clean up code

--- src/pages/todos/TodosLayout.jsx
+++ src/pages/todos/TodosLayout.jsx
@@ -1,4 +1,4 @@
-import { NavLink, Outlet, useParams } from 'react-router-dom';
+import { Outlet } from 'react-router-dom';
 import styles from './TodosLayout.module.css';
 
 import TodosSidebar from './TodosSidebar';
