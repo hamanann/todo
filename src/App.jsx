42d9593c0b1c4643f50009d49da2857f9f3f15ca
aagic1 <aagic1@etf.unsa.ba>
2023-07-06 16:28:38 +0200

    Create basic page that lists todos

--- src/App.jsx
+++ src/App.jsx
@@ -8,13 +8,14 @@ import {
 
 import MainLayout from './components/MainLayout';
 import TodosLayout from './pages/todos/TodosLayout';
+import Todos, { loader as todosLoader } from './pages/todos/Todos';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path='/' element={<MainLayout />}>
       <Route index element={<h2>Home Page</h2>} />
       <Route path='todos' element={<TodosLayout />}>
-        <Route path='general' element={<h2>general - todolist</h2>} />
+        <Route path='general' element={<Todos />} loader={todosLoader} />
         <Route path='today' element={<h2>today - todolist</h2>} />
         <Route path='upcoming' element={<h2>upcoming - todolist</h2>} />
         <Route path='projects' element={<h2>all projects</h2>} />
