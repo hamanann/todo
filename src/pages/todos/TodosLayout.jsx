eb3fe53f44fd94cde26228b634c2e10bee7fa3a3
aagic1 <aagic1@etf.unsa.ba>
2023-07-09 22:55:33 +0200

    Navigate user when visiting /todos url

--- src/pages/todos/TodosLayout.jsx
+++ src/pages/todos/TodosLayout.jsx
@@ -1,18 +1,21 @@
-import { Outlet } from 'react-router-dom';
+import { Navigate, Outlet } from 'react-router-dom';
 import styles from './TodosLayout.module.css';
 
 import TodosSidebar from './TodosSidebar';
 
 export default function TodosLayout() {
   return (
-    <div className={styles.container}>
-      <TodosSidebar />
+    <>
+      <Navigate to='today' />
+      <div className={styles.container}>
+        <TodosSidebar />
 
-      <section className={styles.mainContainer}>
-        <div className={styles.mainContent}>
-          <Outlet />
-        </div>
-      </section>
-    </div>
+        <section className={styles.mainContainer}>
+          <div className={styles.mainContent}>
+            <Outlet />
+          </div>
+        </section>
+      </div>
+    </>
   );
 }
